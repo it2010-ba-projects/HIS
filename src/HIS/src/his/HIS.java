8d0a4065fa016d04e2efbaa35926c1e690774ff6
kernprojekt <kirschkern_sis@web.de>
2011-10-26 12:50:20 +0200

    [FIXES #20124885] Implementation des Fensterkonzeptes

---
+++ src/HIS/src/his/HIS.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package his;
+
+import his.ui.views.MainForm;
+
+/**
+ *
+ * @author Satzu
+ */
+public class HIS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainForm main = new MainForm();
+        main.setVisible(true);
+        
+    }
+}
