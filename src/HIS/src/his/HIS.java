f1455bce1319f4b412bd526b42c26db8eebc9169
Thomas Schulze <thomas.schulze@it2010.ba-leipzig.de>
2011-11-02 17:32:49 +0100

    [#20487957] Logging mit log4j implementiert und in Klasse HIS als statisches Objekt bereit gestellt

--- src/HIS/src/his/HIS.java
+++ src/HIS/src/his/HIS.java
@@ -20,18 +20,28 @@
 package his;
 
 import his.ui.views.MainForm;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 
 /**
  *
  * @author Franziska Staake
  */
 public class HIS {
-
+    private static Logger logger = Logger.getRootLogger();
+    
+    /**
+     * @return the logger
+     */
+    public static Logger getLogger() {
+        return logger;
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        logger.setLevel(Level.DEBUG);
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -54,8 +64,8 @@ public class HIS {
             java.util.logging.Logger.getLogger(MainForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-        
-        MainForm main = new MainForm();
+        logger.debug("Starte MainForm");
+        MainForm main = new MainForm();        
         main.setVisible(true);
         
     }
