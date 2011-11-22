7aa32f7abeb048bc92cbb82fff9949e840d754ae
Thomas Schulze <thomas.schulze@it2010.ba-leipzig.de>
2011-11-22 20:06:12 +0100

    [#20943631] Linzenz und Autor hinzugefuegt

--- src/HIS/src/his/ui/events/CategoriesResultShowListener.java
+++ src/HIS/src/his/ui/events/CategoriesResultShowListener.java
@@ -1,6 +1,21 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+    Copyright 2011 Silvio Wehner, Franziska Staake, Thomas Schulze
+  
+    This file is part of HIS.
+
+    HIS is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    HIS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with HIS.  If not, see <http://www.gnu.org/licenses/>.
+ 
  */
 package his.ui.events;
 
@@ -8,7 +23,7 @@ import java.util.EventListener;
 
 /**
  *
- * @author Thomas
+ * @author Thomas Schulze
  */
 public interface CategoriesResultShowListener extends EventListener {
     public void categoriesResultShowPerformed(CategoriesResultShowEvent evt);
